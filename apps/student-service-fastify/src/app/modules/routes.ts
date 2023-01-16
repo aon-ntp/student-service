cfe9ffa7b4a7d6578ccf9e79a40f9811decd3725
nonthawat <nonthawat.pae@cpf.co.th>
2023-01-16 15:52:04 +0700

    refactor(commit): refactor in route

--- apps/student-service-fastify/src/app/modules/routes.ts
+++ apps/student-service-fastify/src/app/modules/routes.ts
@@ -1,6 +1,7 @@
 import profileRoutes from "./profile/route"
 import customerRoutes from "./customer/route";
 import { FastifyInstance } from "fastify";
+import { courseLookupRoutes } from "./course-lookup/route";
 
 
 export default function register(server:FastifyInstance):FastifyInstance{
@@ -9,7 +10,7 @@ server.register(profileRoutes,{prefix:"/api/profile"})
 // server.register(userRoutes,{prefix:"/api/user"})
 // server.register(productRoutes,{prefix:"/api/product"})
 server.register(customerRoutes,{prefix:"/api/customer"})
-
+server.register(courseLookupRoutes,{prefix: "/api/course-lookup"})
 return server
 }
 
